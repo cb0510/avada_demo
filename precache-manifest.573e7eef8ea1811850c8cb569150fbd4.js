66f701b29237f5429bedc9656642605f49bdcb70
cb0510 <34616485+cb0510@users.noreply.github.com>
2019-04-08 16:35:47 +0700

    Initial Commit
    Initial Commit

---
+++ precache-manifest.573e7eef8ea1811850c8cb569150fbd4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/avada_demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b7d8966bcd535a1b348a",
+    "url": "/avada_demo/static/css/main.9a93f53b.chunk.css"
+  },
+  {
+    "revision": "a20ee798e14206c0f9b4",
+    "url": "/avada_demo/static/js/runtime~main.a20ee798.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/avada_demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "262d263030a138b1a4ef",
+    "url": "/avada_demo/static/js/2.262d2630.chunk.js"
+  },
+  {
+    "revision": "b7d8966bcd535a1b348a",
+    "url": "/avada_demo/static/js/main.b7d8966b.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/avada_demo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/avada_demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/avada_demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "262d263030a138b1a4ef",
+    "url": "/avada_demo/static/css/2.c7473694.chunk.css"
+  },
+  {
+    "revision": "939c5f2a5dacaf1667a1e3a0adaa9e54",
+    "url": "/avada_demo/index.html"
+  }
+];
